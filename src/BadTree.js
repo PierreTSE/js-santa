397e8ebecd827be4e404020f9fba56a2602c3f9b
PierreTSE <pierre--giraud@orange.fr>
2019-01-21 22:09:40 +0100

    fixed image location

--- src/BadTree.js
+++ src/BadTree.js
@@ -3,7 +3,7 @@ class BadTree extends Tree {
      * {@link Entity#constructor}
      */
     constructor(canvasWidth, canvasHeight) {
-        super("../rc/bad_tree.png", 2, 1, 1, canvasWidth, canvasHeight);
+        super("../rc/images/bad_tree.png", 2, 1, 1, canvasWidth, canvasHeight);
 
         this.LONGEVITY = 20000; // in ms
         this.SPAWNED_ELVES = 1;
