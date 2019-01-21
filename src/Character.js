28dc2be5ec30f373d0c3b7d18c48fb19af6554a5
PierreTSE <pierre--giraud@orange.fr>
2019-01-21 03:31:32 +0100

    removed redundant move function

--- src/Character.js
+++ src/Character.js
@@ -13,24 +13,12 @@ class Character extends Entity {
     }
 
     /**
-     * Moves the sprite by the given deltas.
-     * @param dx x value to move
-     * @param dy y value to move
-     * @param canvasWidth Width of the game canvas
-     * @param canvasHeight Height of the game canvas
-     */
-    move(dx, dy, canvasWidth, canvasHeight) {
-        this.x = clamp(this.x + dx, 0, canvasWidth - this.spriteWidth * this.horatio);
-        this.y = clamp(this.y + dy, 0, canvasHeight - this.spriteHeight * this.horatio);
-    }
-
-    /**
      * Draws the Character sprite on the given context.
      * @param context The context to draw onto.
      */
     draw(context) {
         const sx = this.spriteWidth * 1; //TODO implémenter animation
         const sy = this.spriteHeight * (this.orientation - 1);
-        context.drawImage(this.spritesheet, sx, sy, this.spriteWidth, this.spriteHeight, this.x, this.y, this.spriteWidth * this.horatio, this.spriteHeight * this.horatio);
+        context.drawImage(this.spritesheet, sx, sy, this.spriteWidth, this.spriteHeight, this.x, this.y, this.WIDTH, this.HEIGHT);
     }
 }
