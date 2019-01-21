b19ba118e5d8fbd408d37a79ee1fa2b7054ad8a6
PierreTSE <pierre--giraud@orange.fr>
2019-01-21 22:07:06 +0100

    formatting

--- src/UnanimatedEntity.js
+++ src/UnanimatedEntity.js
@@ -4,18 +4,15 @@ class UnanimatedEntity extends Entity {
      */
     constructor(spriteSource, nX, nY, horatio) {
         super(spriteSource, nX, nY, horatio);
-        this.spriteShift=randint(0, nX-1); //choose one sprite among the possible ones
+        this.spriteShift = randint(0, nX - 1); //choose one sprite among the possible ones
 
     }
-
 
     /**
      * Draws the AnimatedEntity sprite on the given context.
      * @param context The context to draw onto.
      */
     draw(context) {
-        // TODO draw
-         context.drawImage(this.spritesheet, this.spriteShift*this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.WIDTH, this.HEIGHT);
+        context.drawImage(this.spritesheet, this.spriteShift * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.WIDTH, this.HEIGHT);
     }
-
 }
